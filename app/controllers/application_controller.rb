4c0a516736b7d0bf3ddc1ec6f5c6a27974f7ecc5
amanda <amanda.blaze1988@gmail.com>
2021-02-24 17:00:21 -0800

    Created Welcome.erb in views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,28 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
   end
 
   get "/" do
     erb :welcome
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        "Sorry it looks like your not logged in!"
+        redirect to '/users/login'
+      end
+    end
+  end
 end
