a4954c6cd878467ed9fb441c6f0954ed9d1e96f5
amanda <amanda.blaze1988@gmail.com>
2021-02-28 17:22:56 -0800

    Created forms in Users.erb, Books.erb and Reviews

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_230843) do
+ActiveRecord::Schema.define(version: 2021_02_27_010151) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.integer "release_date"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.string "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
 end
