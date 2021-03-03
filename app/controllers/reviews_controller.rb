3c6fc77eb935c02d8c70decac9761c4b45b4e6dd
amanda <amanda.blaze1988@gmail.com>
2021-03-03 15:20:45 -0800

    Made changes to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,63 +18,30 @@ class ReviewsController < ApplicationController
     end
   
     get '/reviews/:id' do
-      redirect_if_not_logged_in
+      @review = Review.find_by(id:params[:id])
+      erb :'reviews/show'
+     end
 
-    @user = current_user
-    @review = Review.find_by_id(params[:id])
-    @book = Book.find_by_id(@review.book_id)
-    erb :'/reviews/show'
-    
-  end
-  
     get '/reviews/:id/edit' do
-      redirect_if_not_logged_in
-  
-      @review = Review.find_by_id(params[:id])
-      if @review.user == current_user
-        @books = Book.all
-        erb :'/reviews/edit'
+      @review = Review.find_by(id:params[:id])
+        erb :'reviews/edit'
       else
         redirect to "/reviews/#{params[:id]}"
       end
     end
   
     patch '/reviews/:id' do
-      redirect_if_not_logged_in
+      @review = Review.find_by(id:params[:id])
+      @review.update(title: params[:title], comments: params[:comments])
+      redirect "/reviews/#{@review.id}"
+    end 
   
-      @review = Review.find_by_id(params[:id])
-      @book = Book.find_by_id(@review.book_id)
-  
-      if @review.invalid?
-        flash[:error] =  "Oops! Make sure to fill out all criteria!"
-        erb :'/reviews/edit'
-      else
-        if @review && @review.user == current_user
-          if params[:new_book_name].empty?
-            @book.update(name: params[:book_name])
-          else
-            @book.update(name: params[:new_book_name])
-          end
-  
-          @review.update( book_id: @book.id, user_id: current_user.id)
-          flash[:error] =  "Review successfully updated."
-          redirect to "/reviews/#{params[:id]}"
-        else
-          redirect to '/reviews'
-        end
-      end
-    end
+     
   
     delete '/reviews/:id/delete' do
-      redirect_if_not_logged_in
-  
-      @review = Review.find_by_id(params[:id])
-      if @review && @review.user == current_user
-        @review.delete
-        flash[:error] = "Review has been deleted."
-        redirect to "/users/#{@review.user_id}"
-      else
-        redirect to "/reviews/#{params[:id]}"
+        @review = Review.find_by(id:params[:id])
+        @review.destroy
+        redirect to "/reviews"
       end
     end
   end
--- config.ru
+++ config.ru
@@ -1,6 +1,6 @@
 require './config/environment'
 
- 
+use Rack::MethodOverride
 use ReviewsController
 use BooksController
 use UsersController
