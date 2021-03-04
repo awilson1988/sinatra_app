78ea49bd9e16d6c5939cd4a742be34b1c845a217
amanda <amanda.blaze1988@gmail.com>
2021-03-04 13:30:53 -0800

    Created Book/index.erb

--- db/migrate/seeds.rb
+++ db/migrate/seeds.rb
@@ -1,1 +1,21 @@
 User.create(username: 'ablaze88', email: 'amanda.blaze1988@gmail.com', password: 'password')
+
+Book.create(title: 'Don Quixote', author: 'Miguel de Cervantes Saavedra')
+
+Book.create(title: 'Call Me by Your Name: A Novel', author: 'Andre Aciman') 
+
+Book.create(title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou') 
+
+Book.create(title: 'The Year of Magical Thinking', author: 'Joan Didion') 
+
+Book.create(title: 'Misery: A Novel', author: 'Stephen King')
+
+Book.create(title: 'Hood Feminism: Notes from the Women That a Movement Forgot', author: 'Mikki Kendall')
+
+Book.create(title: 'The Vanishing Half: A Novel', author: 'Brit Bennett')
+
+Book.create(title: 'The Deep', author: 'Jonathan Snipes')
+
+Book.create(title: 'Pretty Girls', author: 'Karin Slaughter')
+
+Book.create(title: 'Circe', author: 'Madeline Miller')
