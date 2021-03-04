a2c96b9c56fc6a52be6bd0fa25df96cef3c291b1
amanda <amanda.blaze1988@gmail.com>
2021-03-04 14:18:16 -0800

    Added to layout.erb nav bar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,8 +4,6 @@ class BooksController < ApplicationController
     end
   
     get '/books/:id' do
-      redirect_if_not_logged_in
-  
       @user = current_user
       @book = Book.find_by_id(params[:id])
       erb :'/books/show'
