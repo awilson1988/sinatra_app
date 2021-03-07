7d62cbf71a66773700ed8649b8f0794841cd8818
amanda <amanda.blaze1988@gmail.com>
2021-03-06 22:38:09 -0800

    Edited views.erb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,26 @@
 class ReviewsController < ApplicationController
-    get '/reviews' do
-      @reviews = Review.all 
-      erb :'reviews/index'
-    end
+  
+  get '/reviews' do
+    @reviews = Review.all 
+    erb :'reviews/index'
+  end
   
     get '/reviews/new' do
       erb :'reviews/new'
     end
-  
-    post '/reviews' do
     
-      @review = Review.new(params) 
-      @review.user_id = session[:user_id] 
-      @review.save
-      redirect "/reviews/#{@review.id}" 
-      
-    end
-  
     get '/reviews/:id' do
-      @review = Review.find_by(id:params[:id])
+      @reviews = Review.find_by(id:params[:id])
       erb :'reviews/show'
-     end
+    end
+    
+      post '/reviews' do
+        review = Review.new(params) 
+        review.user_id = session[:user_id] 
+        review.save
+        redirect "/reviews/#{review.id}" 
+        
+      end
 
     get '/reviews/:id/edit' do
       @review = Review.find_by(id:params[:id])
@@ -35,10 +35,12 @@ class ReviewsController < ApplicationController
   
      
   
-    delete '/reviews/:id' do
-        @review = Review.find_by(id:params[:id])
-        @review.delete
-        redirect to "/reviews"
-      end
-    end
+  delete '/reviews/:id' do
+    review = Review.find_by(id:params[:id])
+    review.delete
+    redirect "/reviews"
+  end
+
+end 
+  
   
