ce231f823fcc1ee33e5697d1721733d197d5c7b2
amanda <amanda.blaze1988@gmail.com>
2021-03-07 18:56:18 -0800

    updated user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+ 
+ 
+  #render signup form to create user
   get '/users/signup' do
     if logged_in?
       redirect "/users/#{@user.id}"
@@ -7,12 +10,14 @@ class UsersController < ApplicationController
     end
   end
 
+  
+  #create user
   post '/signup' do
-    @user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
-    if @user.valid?
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    elsif @user.invalid? && User.find_by(username: @user.username)
+    user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+    if user.valid?
+      session[:user_id] = user.id
+      redirect "/users/#{user.id}"
+    elsif user.invalid? && User.find_by(username: user.username)
       flash[:error] = "Sorry, that username is already taken."
       erb :'/users/signup'
     else
@@ -21,40 +26,32 @@ class UsersController < ApplicationController
     end
   end
 
-  get '/users/login' do
-    if logged_in?
-      session[:user_id] = @user.id
-      redirect "/users/#{@user.id}"
-    else
-      erb :'/users/login'
-    end
-  end
-
-  post '/login' do
-    @user = User.find_by(username: params[:username])
-
-    if @user && @user.authenticate(params[:password])
-      session[:user_id] = @user.id
-      flash[:error] = "Welcome back, #{@user.username}!"
-      redirect "/users/#{@user.id}"
-    else
-      flash[:error] = "There was trouble logging in. Please try again."
-      redirect to '/users/login'
-    end
-  end
-
+  #show user profile
   get '/users/:id' do
     @user = User.find_by(id:params[:id])
     @books = Book.all
     erb :'/users/show'
   end
 
-  get '/logout' do
-    if logged_in?
-      session.clear
-      redirect '/'
-    else
-      redirect '/'
-    end
+  #renders form to edit user
+  get '/users/:id/edit' do 
+    @user = User.find_by(id: params[:id])
+    erb :'/users/edit'
   end
+
+  #updates user
+  patch '/users/:id' do
+    user = current_user
+    user.update(params["user"])
+    redirect "/users/#{user.id}"
+  end
+
+  #deletes existing user
+  delete '/users/:id' do
+  user = User.find_by(id: params[:id])
+  user.destroy
+  session.clear
+  redirect "/"
+  end
+
 end
