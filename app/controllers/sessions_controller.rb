e0582413e3c1588f06253edec0d87682f4ad6454
amanda <amanda.blaze1988@gmail.com>
2021-03-07 18:55:30 -0800

    sessions_controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Sessions_Controller < ApplicationController
+  
+    
+    post '/login' do
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        flash[:success] = "Welcome back, #{user.username}!"
+        redirect "/users/#{user.id}"
+      else
+        flash[:error] = "There was trouble logging in. Please try again."
+        redirect to '/users/login'
+      end
+    end
+  
+    get '/logout' do
+      if logged_in?
+        session.clear
+        redirect '/'
+      else
+        redirect '/'
+      end
+    end
+  
+  
+  end
