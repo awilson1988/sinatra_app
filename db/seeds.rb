e75819c3f6aaea8221947b113e039c06d644bad0
amanda <amanda.blaze1988@gmail.com>
2021-03-08 21:51:47 -0800

    Added summaries to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,41 @@
 User.create(username: 'ablaze88', email: 'amanda.blaze1988@gmail.com', password: 'password')
 
-Book.create(title: 'Don Quixote', author: 'Miguel de Cervantes Saavedra')
+Book.create(title: 'Don Quixote', author: 'Miguel de Cervantes Saavedra', summary: 'Don Quixote is a middle-aged 
+gentleman from the region of La Mancha in central Spain. Obsessed with the chivalrous ideals touted in books he has 
+read, he decides to take up his lance and sword to defend the helpless and destroy the wicked.')
 
-Book.create(title: 'Call Me by Your Name: A Novel', author: 'Andre Aciman') 
+Book.create(title: 'Call Me by Your Name: A Novel', author: 'Andre Aciman', summary: 'Image result for call me by your name book summary
+Call Me by Your Name is the story of a sudden and powerful romance that blossoms between an adolescent boy and a summer guest at 
+his parents cliff-side mansion on the Italian Riviera. Unprepared for the consequences of their attraction, at first each feigns indifference.') 
 
-Book.create(title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou') 
+Book.create(title: 'I Know Why the Caged Bird Sings', author: 'Maya Angelou', summary: 'In I Know Why the Caged Bird Sings, Maya Angelou 
+describes her coming of age as a precocious but insecure black girl in the American South during the 1930s and subsequently 
+in California during the 1940s.') 
 
-Book.create(title: 'The Year of Magical Thinking', author: 'Joan Didion') 
+Book.create(title: 'The Year of Magical Thinking', author: 'Joan Didion', summary: 'The Year of Magical Thinking is Joan Didions 
+account of the year following the death of her husband, writer John Gregory Dunne, and her attempts to make sense of her grief 
+while tending to the severe illness of her adopted daughter, Quintana.') 
 
-Book.create(title: 'Misery: A Novel', author: 'Stephen King')
+Book.create(title: 'Misery: A Novel', author: 'Stephen King', summary: 'The novels narrative is based on the relationship 
+of its two main characters – the popular writer Paul Sheldon and his psychotic fan Annie Wilkes. When Paul is seriously 
+injured following a car accident, former nurse Annie brings him to her home, where Paul receives treatment and doses of pain medication.')
 
-Book.create(title: 'Hood Feminism: Notes from the Women That a Movement Forgot', author: 'Mikki Kendall')
+Book.create(title: 'Hood Feminism: Notes from the Women That a Movement Forgot', author: 'Mikki Kendall', summary: 'Hood Feminism Is 
+A Call For Solidarity In Less-Than-Inclusive Movement Mikki Kendall reveals how feminism has failed to consider populations 
+too often excluded from the movements banner — and forgotten to weigh the breadth of issues affecting the daily lives of many.')
 
-Book.create(title: 'The Vanishing Half: A Novel', author: 'Brit Bennett')
+Book.create(title: 'The Vanishing Half: A Novel', author: 'Brit Bennett', summary: 'Looking well beyond issues of race, 
+The Vanishing Half considers the lasting influence of the past as it shapes a persons decisions, desires, and expectations, 
+and explores some of the multiple reasons and realms in which people sometimes feel pulled to live as something other than their origins.')
 
-Book.create(title: 'The Deep', author: 'Jonathan Snipes')
+Book.create(title: 'The Deep', author: 'Jonathan Snipes', summary: 'The Deep is a 2019 fantasy book by Rivers Solomon, 
+with Daveed Diggs, William Hutson and Jonathan Snipes. It depicts a utopian underwater society built by the water-breathing 
+descendants of pregnant slaves thrown overboard from slave ships.')
 
-Book.create(title: 'Pretty Girls', author: 'Karin Slaughter')
+Book.create(title: 'Pretty Girls', author: 'Karin Slaughter', summary: 'Sisters Lydia Delgado and Claire Scott havent spoken 
+in over twenty years. After the disappearance of their nineteen-year-old sister, Julia, their family essentially fell apart. 
+Further events jammed a wedge so deep between the sisters they went on to lead separate lives completely void of each other.')
 
-Book.create(title: 'Circe', author: 'Madeline Miller')
+Book.create(title: 'Circe', author: 'Madeline Miller', summary: 'Despised by her divine family, Circe discovers her powers of 
+sorcery when she turns a human fisherman into a god. When he spurns her for another nymph, Scylla, Circe transforms her rival 
+into a horrific sea monster who becomes the sourge of all sailors – an act that will haunt Circe for the rest of her life.')
